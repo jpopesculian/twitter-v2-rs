a2b998c92f645288c1ae9a0121912e3b6ee3c57e
totto2727 <kaihatu.totto2727@gmail.com>
2022-11-30 14:46:37 +0900

    fix MachingRule struct, id was changed NumericId from String and tag was changed Option<String> from String.

--- src/data/matching_rule.rs
+++ src/data/matching_rule.rs
@@ -1,7 +1,9 @@
+use crate::id::NumericId;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct MatchingRule {
-    pub id: String,
-    pub tag: String,
+    pub id: NumericId,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tag: Option<String>,
 }
